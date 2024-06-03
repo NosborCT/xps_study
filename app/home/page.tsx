c0455916d7520fe7908aec7a2cddab2089867d1b
NosborCT <hainiken08@gmail.com>
2024-06-03 11:42:13 -0300

    feat: home page footer

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,11 @@
+import Footer from "../_components/footer";
 import Header from "../_components/header";
 import { Card } from "../_components/ui/card";
 import { Separator } from "../_components/ui/separator";
 
 const Home = () => {
   return (
-    <div>
+    <div className=" overflow-auto">
       <Header />
       <div className="flex justify-center py-6">
         <Separator className="w-[360px]"></Separator>
@@ -12,14 +13,21 @@ const Home = () => {
       <h3 className="text-white flex items-center justify-center py-6">
         Atividades do dia
       </h3>
-      <div className="flex-col space-y-6 flex items-center min-h-screen py-6">
-        <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
-        <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
-        <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
-        <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
-        <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
-        <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+
+      <div className="flex-col space-y-6 flex items-center ">
+        <div className="flex-col space-y-6">
+          <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+          <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+          <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+          <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+          <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+          <Card className="w-80 h-20 cursor-pointer bg-primary"></Card>
+        </div>
+        
       </div>
+      <div className=" pt-24">
+            <Footer />
+        </div>
     </div>
   );
 };
